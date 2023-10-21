146266982adc47680c9bbe97dc48f792b2af4ebf
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-22 00:20:15 +0300

    Кнопка выйти

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,23 @@
+import { Button } from 'antd';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import { SelectProfile, logoutAccount } from '../redux/slices/profileSlice';
+import { Status } from '../models/Status.enum';
+import CreatePollForm from '../components/Forms/CreatePoll';
 
 type Props = {};
 
 const Profile = (props: Props) => {
-  return <div>Profile</div>;
+  const { status } = useAppSelector(SelectProfile);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <Button loading={status === Status.LOADING} danger onClick={() => dispatch(logoutAccount())}>
+        Выйти
+      </Button>
+    </div>
+  );
 };
 
 export default Profile;
