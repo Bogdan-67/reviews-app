b74a19015d330be360d7ff2d1314069f6d25b5b5
Chynarich <chynarich1@gmail.com>
2023-10-22 17:33:03 +0300

    Получение опроса по айди

--- backend/api/routes/polls.routes.js
+++ backend/api/routes/polls.routes.js
@@ -2,6 +2,7 @@ const Router = require('express');
 const router = new Router();
 const pollsController = require('../controller/polls.controller.js');
 
+router.get('/getPoll/:id_poll', pollsController.getPoll);
 router.get('/question-types', pollsController.getQuestionTypes);
 router.post('/create-poll', pollsController.createPoll);
 
