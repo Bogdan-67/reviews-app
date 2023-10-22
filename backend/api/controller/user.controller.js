a9fd5a4fafe92aa1ef034c5f114ae76d6380a75a
Chynarich <chynarich1@gmail.com>
2023-10-22 15:43:40 +0300

    добавил нахождение пользователя по ролям

--- backend/api/controller/user.controller.js
+++ backend/api/controller/user.controller.js
@@ -32,6 +32,15 @@ class UserController {
       next(e);
     }
   }
+  async getUserOnRules(req, res, next) {
+    try {
+      const users = await userService.getUserOnRules(req.body);
+      res.status(200).json(users);
+    } catch (e) {
+      await db.query('ROLLBACK');
+      next(e);
+    }
+  }
 }
 
 module.exports = new UserController();
