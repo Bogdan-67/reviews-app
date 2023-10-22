461aefa13fb1c14bab43bd755f970fc432b3ee15
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-10-22 09:21:21 +0300

    Переделана выдача и проверка ролей

--- backend/api/routes/role.routes.js
+++ backend/api/routes/role.routes.js
@@ -3,8 +3,12 @@ const router = new Router();
 const roleController = require('../controller/role.controller');
 const checkRole = require('../middlewares/check-role-middleware');
 
-router.get('/roles', checkRole('ADMIN'), roleController.getRoles);
-router.post('/roles', checkRole('ADMIN'), roleController.giveRole);
-router.post('/remove-roles', checkRole('ADMIN'), roleController.removeRole);
+router.get('/roles', roleController.getRoles);
+router.post('/roles', checkRole('Руководитель'), roleController.giveRole);
+router.post(
+  '/remove-roles',
+  checkRole('Руководитель'),
+  roleController.removeRole
+);
 
 module.exports = router;
