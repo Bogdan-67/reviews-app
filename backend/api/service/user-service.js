8bb0921d5064cf36ce583df4a832f6a7916672b6
Chynarich <chynarich1@gmail.com>
2023-10-22 09:02:38 +0300

    Аккаунты приходят вместе с юзерами

--- backend/api/service/user-service.js
+++ backend/api/service/user-service.js
@@ -11,6 +11,29 @@ class UserService {
     const usersObjects = [];
 
     for (const row of usersRows) {
+      const user_roles = await db.query(
+        `SELECT user_roles.*
+      FROM user_roles
+      INNER JOIN accounts ON user_roles.account_id = accounts.id_account
+      WHERE accounts.user_id = $1;
+      `,
+        [row.id_user]
+      );
+
+      const userRolesObjects = [];
+
+      for (const row of user_roles.rows) {
+        const roleName = await db.query(
+          `SELECT role_name FROM roles WHERE id_role = $1;
+          `,
+          [row.role_id]
+        );
+        userRolesObjects.push({
+          role_id: row.role_id,
+          role_name: roleName.rows[0].role_name,
+        });
+      }
+
       const usersObject = {
         id_user: row.id_user,
         firstname: row.firstname,
@@ -19,6 +42,7 @@ class UserService {
         phone: row.phone,
         email: row.email,
         rating: row.rating,
+        roles: userRolesObjects,
       };
 
       usersObjects.push(usersObject);
