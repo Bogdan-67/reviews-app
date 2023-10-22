4b01d958024cc67a9d2d9762304c29b7c977ffe1
RaKeD <orlovdanorlov@gmail.com>
2023-10-22 13:23:52 +0300

    В ответе на всех пользователей добавлено поле teams

--- backend/api/service/user-service.js
+++ backend/api/service/user-service.js
@@ -1,52 +1,64 @@
 const db = require('../db');
 const ApiError = require('../exceptions/api-error');
-
 class UserService {
   async getUsers() {
     await db.query('BEGIN');
 
     const users = await db.query(`SELECT * FROM users`);
-
     const usersRows = users.rows;
     const usersObjects = [];
 
-    for (const row of usersRows) {
+    for (const userRow of usersRows) {
       const user_roles = await db.query(
         `SELECT user_roles.*
       FROM user_roles
       INNER JOIN accounts ON user_roles.account_id = accounts.id_account
       WHERE accounts.user_id = $1;
       `,
-        [row.id_user]
+        [userRow.id_user],
       );
 
       const userRolesObjects = [];
 
-      for (const row of user_roles.rows) {
+      for (const roleRow of user_roles.rows) {
         const roleName = await db.query(
           `SELECT role_name FROM roles WHERE id_role = $1;
-          `,
-          [row.role_id]
+        `,
+          [roleRow.role_id],
         );
         userRolesObjects.push({
-          role_id: row.role_id,
+          role_id: roleRow.role_id,
           role_name: roleName.rows[0].role_name,
         });
       }
 
-      const usersObject = {
-        id_user: row.id_user,
-        firstname: row.firstname,
-        lastname: row.lastname,
-        middlename: row.middlename,
-        phone: row.phone,
-        email: row.email,
-        rating: row.rating,
+      // Дополнительный SQL-запрос для получения команд пользователя
+      const userTeams = await db.query(
+        `SELECT teams.team_name
+      FROM teams
+      INNER JOIN user_teams ON teams.id_team = user_teams.team_id
+      WHERE user_teams.user_id = $1;
+      `,
+        [userRow.id_user],
+      );
+
+      const userTeamsArray = userTeams.rows.map((row) => row.team_name);
+
+      const userObject = {
+        id_user: userRow.id_user,
+        firstname: userRow.firstname,
+        lastname: userRow.lastname,
+        middlename: userRow.middlename,
+        phone: userRow.phone,
+        email: userRow.email,
+        rating: userRow.rating,
         roles: userRolesObjects,
+        teams: userTeamsArray,
       };
 
-      usersObjects.push(usersObject);
+      usersObjects.push(userObject);
     }
+
     await db.query('COMMIT');
     return usersObjects;
   }
